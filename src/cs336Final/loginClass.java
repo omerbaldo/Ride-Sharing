7f4a8ad56a129346117df55a931fc42525eaefbe
califlower <cal13199@gmail.com>
2017-04-01 15:24:17 -0400

    Added User Manager Page for Admin

--- src/cs336Final/loginClass.java
+++ src/cs336Final/loginClass.java
@@ -12,22 +12,12 @@ public class loginClass
 	public static boolean login (String username, String password)
 	{
 		try
-		{
-			
-		
-			String url = "jdbc:mysql://cs336dbinstance.cxvvrbjkmr4a.us-west-2.rds.amazonaws.com:3306";
-			//Load JDBC driver - the interface standardizing the connection procedure. Look at WEB-INF\lib for a mysql connector jar file, otherwise it fails.
-			Class.forName("com.mysql.jdbc.Driver");
-
-			//Create a connection to your DB
-			Connection con = DriverManager.getConnection(url, "omerdeepcal", "wegotthis");
-			//Create a SQL statement
-			Statement stmt = con.createStatement();
-
-			//Populate SQL statement with an actual query. It returns a single number. The number of beers in the DB.
-			String str = "SELECT user_id FROM app.User where password = '"+password.trim() +"' and username = '"+username.trim()+"'";
+		{	String str = "SELECT user_id FROM app.User where password = '"+password.trim() +"' and username = '"+username.trim()+"'";
 			//Run the query against the DB
-			ResultSet result = stmt.executeQuery(str);
+		
+			
+			ResultSet result = sql.query(str).get();
+			
 			
 			
 			if (getRows(result) == 0)
