fdd87e0d53bc297e246eb3b718abd4b455d4c5b1
califlower <cal13199@gmail.com>
2017-04-04 12:09:11 -0400

    expanded user object

--- src/cs336Final/loginClass.java
+++ src/cs336Final/loginClass.java
@@ -54,8 +54,10 @@ public class loginClass
 			
 			String e = result.getString(5);
 			String add = result.getString(6);
+			String phone = result.getString(7);
+			int age = result.getInt(8);
 			int x = result.getInt(9);
-			return new LoginObject(a, b , n , p , e , add, x);
+			return new LoginObject(a, b , n , p , e , add, phone, age, x);
 			
 			
 		}
