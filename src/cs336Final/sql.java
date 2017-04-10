1b31fe0cddd62ce0a8c061179888f48e6c61c194
califlower <cal13199@gmail.com>
2017-04-09 20:12:38 -0400

    can lock a user

--- src/cs336Final/sql.java
+++ src/cs336Final/sql.java
@@ -39,5 +39,36 @@ public class sql
 		}
 		
 	}
+	
+	public static int upd(String q)
+	{
+		try
+		{
+			String url = "jdbc:mysql://cs336dbinstance.cxvvrbjkmr4a.us-west-2.rds.amazonaws.com:3306";
+			//Load JDBC driver - the interface standardizing the connection procedure. Look at WEB-INF\lib for a mysql connector jar file, otherwise it fails.
+			Class.forName("com.mysql.jdbc.Driver");
+	
+			//Create a connection to your DB
+			Connection con = DriverManager.getConnection(url, "omerdeepcal", "wegotthis");
+			//Create a SQL statement
+			Statement stmt = con.createStatement();
+	
+			//Populate SQL statement with an actual query. It returns a single number. The number of beers in the DB.
+	
+			//Run the query against the DB
+			int result = stmt.executeUpdate(q);
+			
+			return result;
+
+			
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+			return 0;
+			
+		}
+		
+	}
 
 }
