6ebe36113066db6f6e626c0cb9a6bf5cc6a48c52
Deep Kotadia <deep.kotadia@rutgers.edu>
2017-04-12 01:41:08 -0400

    Add files via upload

--- src/cs336Final/MessageObject.java
+++ src/cs336Final/MessageObject.java
@@ -5,13 +5,14 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class MessageObject {
 
 	public MessageObject() {
 		// TODO Auto-generated constructor stub
 	}
-	public static Boolean sendmsg(LoginObject user, String To, String Subject, String Content, String Date){
+	public static Boolean sendmsg(LoginObject user, String To, String Subject, String Content){
 		try
 		{
 			
@@ -24,10 +25,10 @@ public class MessageObject {
 			//Create a SQL statement
 			Statement stmt = con.createStatement();
 			
-			System.out.println("Trying to insert" + To + Subject + Content + Date);
+			System.out.println("Trying to insert" + To + Subject + Content);
 			String From = Integer.toString(user.getUser_id());
+			String Date = new java.text.SimpleDateFormat("dd/MM/yyyy HH:mm:ss").format(new java.util.Date());
 			 
-			//Populate SQL statement with an actual query. It returns a single number. The number of beers in the DB.
 			String str = "INSERT INTO `app`.`SEND_EMAIL` (`From`, `To`, `Subject`, `Content`, `Date`) VALUES";
 			str += "('" + From + "',";
 			str += "'" + To + "',";
