6e96897f9125d93c0a27522362334effd258fd30
Omer Baldo <omer.s.baldo@gmail.com>
2017-04-14 14:14:00 -0400

    Add files via upload

--- src/cs336Final/carObj.java
+++ src/cs336Final/carObj.java
@@ -12,10 +12,10 @@ public class carObj {
 		// TODO Auto-generated constructor stub
 	}
 	public static Boolean addCar(LoginObject user, String Make, String Model, String Year, String VIM, String n, String LID){
+		
+		LID = LID.trim();
 		try
 		{
-			
-		
 			String url = "jdbc:mysql://cs336dbinstance.cxvvrbjkmr4a.us-west-2.rds.amazonaws.com:3306";
 			//Load JDBC driver - the interface standardizing the connection procedure. Look at WEB-INF\lib for a mysql connector jar file, otherwise it fails.
 			Class.forName("com.mysql.jdbc.Driver");
@@ -30,7 +30,7 @@ public class carObj {
 			 
 			//Populate SQL statement with an actual query. It returns a single number. The number of beers in the DB.
 			String str = "INSERT INTO app.Car(Lincense_Plate, make, model, year,VIM,Number_Seats,UID) VALUES";
-			str += "(" + LID.hashCode() +",";
+			str += "(\"" + LID +"\",";
 			str += "\"" + Make +"\",";
 			str += "\"" + Model +"\",";
 			str += "" + Year + ",";
@@ -132,6 +132,63 @@ public class carObj {
 			return false;
 		}	
 	}
+	
+	
+	
+	
+
+	
+	public static car getCar(String lin){
+		lin = lin.trim();
+		try
+		{
+			String url = "jdbc:mysql://cs336dbinstance.cxvvrbjkmr4a.us-west-2.rds.amazonaws.com:3306";
+			//Load JDBC driver - the interface standardizing the connection procedure. Look at WEB-INF\lib for a mysql connector jar file, otherwise it fails.
+			Class.forName("com.mysql.jdbc.Driver");
+
+			//Create a connection to your DB
+			Connection con = DriverManager.getConnection(url, "omerdeepcal", "wegotthis");
+			//Create a SQL statement
+			Statement stmt = con.createStatement();
+						 
+			//Populate SQL statement with an actual query. It returns a single number. The number of beers in the DB.
+			String str = "Select * From app.Car Where Lincense_Plate Like \"%"
+					+ lin + "%\"";
+				
+			System.out.print(str);
+			//Run the query against the DB
+			ResultSet rs = stmt.executeQuery(str);
+			ArrayList<car> al = new ArrayList<car>();
+			while (rs.next()) {      //for each row in the table   
+				
+				car c = new car(); 
+				for(int i = 1; i < 8;i++) {
+					if(i==1){
+						c.Lincense = rs.getString(i);
+					}else if(i==2){
+						c.Make = rs.getString(i);;
+					}else if(i==3){
+						c.Model = rs.getString(i);;
+					}else if(i==4){
+						c.year = Integer.valueOf(rs.getString(i));
+					}else if (i==7){
+						c.user_id = Integer.valueOf(rs.getString(i));
+					}
+		        }
+				al.add(c);
+			}
+			return al.get(0);
+		}
+		
+		catch (Exception e)
+		{
+			System.out.println(e.toString());
+			return null;
+		}	
+	}
+		
+	
+	
 
 
 	public static class car{
--- src/cs336Final/rideObj.java
+++ src/cs336Final/rideObj.java
@@ -4,15 +4,17 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.Date;
+import java.util.*;
+
+import com.mysql.jdbc.ResultSetMetaData;
+
+import cs336Final.carObj.car;
+
 
 public class rideObj {
 
-	/**
-	 * false if ride is over
-	 */
 	
-	public static boolean addToDB(int userID, String from, String to, String locationfrom, String locationto, String regScheduled, String often){
+	public static boolean addToDB(int userID, String from, String to, String locationfrom, String locationto, String regScheduled, String often, String car){
 
 		try
 		{
@@ -26,14 +28,15 @@ public class rideObj {
 			Statement stmt = con.createStatement();
 
 			//Populate SQL statement with an actual query. It returns a single number. The number of beers in the DB.
-			String str = "INSERT INTO app.Ride (startTime, endTime, scheduled, rating, uid, locationStart, locationEnd) VALUES ("
+			String str = "INSERT INTO app.Ride (startTime, endTime, scheduled, rating, uid, locationStart, locationEnd,car) VALUES ("
 					+ "\" " + from + "\", "
 					+ "\" " + to + "\", "
 					+ "\" " + regScheduled + "\", "
 					+ "0, "
 					+ userID + ", "
 					+ "\" " + locationfrom + "\", "
-					+ "\" " + locationto + "\" )";
+					+ "\" " + locationto + "\", "
+					+ "\" " + car + "\" )";
 		
 			System.out.print(str);
 			//Run the query against the DB
@@ -49,7 +52,251 @@ public class rideObj {
 		}
 	}
 	
+	public static Date convertStringToDateObj(String str){
+		String year = "";
+		int i;
+		for(i = 0; str.charAt(i) != '-';i++){
+			year += str.charAt(i);
+		}
+		i++;
+
+		String month = "";
+		int j = i;
+		for(; str.charAt(j) != '-';j++){
+			month += str.charAt(j);
+		}
+		j++;
+
+		String day = "";
+		int k = j;
+		for(; str.charAt(k) != 'T';k++){
+			day += str.charAt(k);
+		}
+		k++;
+		String hour = "";
+		hour += str.charAt(k);
+		hour += str.charAt(k+1);
+		
+		String minutes = "";
+		minutes += str.charAt(k+3);
+		minutes += str.charAt(k+4);
+		
+		Calendar myCal = Calendar.getInstance();
+
+		myCal.set(Calendar.YEAR, Integer.parseInt(year.trim()));
+		myCal.set(Calendar.MONTH, Integer.parseInt(month)-1);
+		myCal.set(Calendar.DAY_OF_MONTH, Integer.parseInt(day));
+		myCal.set(Calendar.HOUR_OF_DAY, Integer.parseInt(hour));
+		myCal.set(Calendar.MINUTE, Integer.parseInt(minutes));
+		
+		Date theDate = myCal.getTime();
+		
+		return theDate;
+	}
+	
+	
+	
+	
+	public static ArrayList<ride> findRides(String start, String end, String date){
+		try
+		{
+			String url = "jdbc:mysql://cs336dbinstance.cxvvrbjkmr4a.us-west-2.rds.amazonaws.com:3306";
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url, "omerdeepcal", "wegotthis");
+			Statement stmt = con.createStatement();
+			
+			//Step 1. find all cars with locations similar
+			String str = "Select * From app.Ride R Where R.locationStart LIKE '%"
+					+ start
+					+ "%' and R.locationEnd LIKE '%"
+					+ end
+					+ "%'";
+					
+			System.out.println(str);
+
+			ResultSet rs = stmt.executeQuery(str);
+			
+			ArrayList<ride> ridesList = new ArrayList<ride>();
+			
+			while (rs.next()) {      //for each row in the table 
+				ride r = new ride();
+				for(int i = 1; i <= 9;i++) {
+					if(i==1){
+						r.ride_id = Integer.valueOf(rs.getString(i));
+					}else if(i==2){
+						r.start = rs.getString(i);
+					}else if(i==6){
+						r.uid = Integer.valueOf(rs.getString(i));
+					}else if(i==9){
+						r.car = rs.getString(i);
+					}
+		        }
+				//System.out.println("adding car " + r.car);
+				ridesList.add(r);
+			}
+
+
+			//query all results and now check for date
+			
+			for(int i = ridesList.size()-1;i>=0;i--){
+				ride c = ridesList.get(i);
+				Date after = convertStringToDateObj(date);
+				Date ride = convertStringToDateObj(c.start);
+				if(ride.before(after)){
+					ridesList.remove(i);
+				}
+			}
+			
+			
+			return ridesList;
+		}catch (Exception e){
+			System.out.println(e.toString());
+			return null;
+		}	
+		
+		
+	}
+	
 	public static boolean deleteToDB(int rideID){
 		return false;
 	}
+	
+	
+	
+	/**
+	 * @param ride id, user id
+	 * @return primary key of the ride request 
+	 */
+	public static int makeRideRequest(String rid ,int userId){
+		
+		System.out.println("Got here");
+		try
+		{
+			String url = "jdbc:mysql://cs336dbinstance.cxvvrbjkmr4a.us-west-2.rds.amazonaws.com:3306";
+			//Load JDBC driver - the interface standardizing the connection procedure. Look at WEB-INF\lib for a mysql connector jar file, otherwise it fails.
+			Class.forName("com.mysql.jdbc.Driver");
+
+			//Create a connection to your DB
+			Connection con = DriverManager.getConnection(url, "omerdeepcal", "wegotthis");
+			//Create a SQL statement
+			Statement stmt = con.createStatement();
+
+			//Populate SQL statement with an actual query. It returns a single number. The number of beers in the DB.
+			String str = "INSERT INTO app.rideRequest(requester,ride_ID, accepted) VALUES ("
+					+ userId + ","
+					+ rid + ", -1)";
+
+		
+			System.out.print(str);
+			//Run the query against the DB
+			int result = stmt.executeUpdate(str);
+			
+			str = "SELECT MAX(ride_offer) FROM app.rideRequest";
+			ResultSet rs = stmt.executeQuery(str);
+			int requestId = 0;
+			while(rs.next()){
+
+				requestId = Integer.parseInt(rs.getString(1));
+			}
+			return requestId;
+		}
+		
+		catch (Exception e)
+		{
+			System.out.println(e.toString());
+			return 0;
+		}
+	}
+	
+	
+	public static boolean checkAccepted(String rid){
+
+		
+		
+		
+		
+		return false;	
+		
+	}
+	
+	
+	/**
+	 * Get all valid offers for ride
+	 */
+	
+	/*
+	
+	public ArrayList<rideOffer> getOffers(String rid){
+		try
+		{
+			String url = "jdbc:mysql://cs336dbinstance.cxvvrbjkmr4a.us-west-2.rds.amazonaws.com:3306";
+			Class.forName("com.mysql.jdbc.Driver");
+			Connection con = DriverManager.getConnection(url, "omerdeepcal", "wegotthis");
+			Statement stmt = con.createStatement();
+			
+			//Step 1. find all cars with locations similar
+			String str = "Select * From app.rideRequest Where ride_ID = " + rid;
+			ResultSet rs = stmt.executeQuery(str);
+			
+			ArrayList<rideOffer> ridesList = new ArrayList<rideOffer>();
+			
+			while (rs.next()) {      //for each row in the table 
+				ride r = new ride();
+				for(int i = 1; i <= 9;i++) {
+					if(i==1){
+						r.ride_id = Integer.valueOf(rs.getString(i));
+					}else if(i==2){
+						r.start = rs.getString(i);
+					}else if(i==6){
+						r.uid = Integer.valueOf(rs.getString(i));
+					}else if(i==9){
+						r.car = rs.getString(i);
+					}
+		        }
+				//System.out.println("adding car " + r.car);
+				ridesList.add(r);
+			}
+
+
+			//query all results and now check for date
+			
+			for(int i = ridesList.size()-1;i>=0;i--){
+				ride c = ridesList.get(i);
+				Date after = convertStringToDateObj(date);
+				Date ride = convertStringToDateObj(c.start);
+				if(ride.before(after)){
+					ridesList.remove(i);
+				}
+			}
+			
+			
+			return ridesList;
+		}catch (Exception e){
+			System.out.println(e.toString());
+			return null;
+		}	
+	}
+	
+	*/
+	public static class rideOffer{
+		
+		public  int requestID; //ride id			1
+		public  int user_id; //ride id			1
+		public  int leaderBoardRank; //ride id			1
+	}
+	
+	
+	
+	public static class ride{
+		public  String start; //start time		2
+		public  int ride_id; //ride id			1
+		public  int uid;//driver id				6
+		public  String car; //car.				9	
+	
+		public String getCar(){
+			return this.car;
+		}	
+	}
+	
+	
 }
