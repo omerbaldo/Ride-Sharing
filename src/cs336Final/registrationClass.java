8bc91eb1f228a28c49ed5502fd04a2196f11f3bb
Cal <cal13199@gmail.com>
2017-04-16 15:52:26 -0400

    Update registrationClass.java

--- src/cs336Final/registrationClass.java
+++ src/cs336Final/registrationClass.java
@@ -2,6 +2,7 @@ package cs336Final;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
@@ -20,15 +21,58 @@ public class registrationClass
 			//Create a connection to your DB
 			Connection con = DriverManager.getConnection(url, "omerdeepcal", "wegotthis");
 			//Create a SQL statement
-			Statement stmt = con.createStatement();
-
-			//Populate SQL statement with an actual query. It returns a single number. The number of beers in the DB.
-			String str = "INSERT INTO app.User (typeofuser,username,password,email,address,phone_number,age,active) VALUES (1,\""+username+"\",\""+password+"\",\""+email+"\",\""+address+"\",\""+phone+"\",30,1)";
-			System.out.print(str);
-			//Run the query against the DB
-			int result = stmt.executeUpdate(str);
+			
+			PreparedStatement stmt = con.prepareStatement("INSERT INTO app.User (typeofuser,username,password,email,address,phone_number,age,active) VALUES(?,?,?,?,?,?,?,?)");
+			
+			stmt.setInt(1, 3);
+			stmt.setString(2, username);
+			stmt.setString(3, password);
+			stmt.setString(4, email);
+			stmt.setString(5, address);
+			stmt.setString(6, phone);
+			stmt.setInt(7, 30);
+			stmt.setInt(8, 1);
+	
+			stmt.executeUpdate();
+				
+			return true;
+		}
+		
+		catch (Exception e)
+		{
+			System.out.println(e.toString());
+			return false;
+		}
+		
+	}
+	public static boolean registerSupport (String username, String password, String email, String phone, String address)
+	{
+		
+		try
+		{
 			
 		
+			String url = "jdbc:mysql://cs336dbinstance.cxvvrbjkmr4a.us-west-2.rds.amazonaws.com:3306";
+			//Load JDBC driver - the interface standardizing the connection procedure. Look at WEB-INF\lib for a mysql connector jar file, otherwise it fails.
+			Class.forName("com.mysql.jdbc.Driver");
+
+			//Create a connection to your DB
+			Connection con = DriverManager.getConnection(url, "omerdeepcal", "wegotthis");
+			//Create a SQL statement
+			
+			PreparedStatement stmt = con.prepareStatement("INSERT INTO app.User (typeofuser,username,password,email,address,phone_number,age,active) VALUES(?,?,?,?,?,?,?,?)");
+			
+			stmt.setInt(1, 2);
+			stmt.setString(2, username);
+			stmt.setString(3, password);
+			stmt.setString(4, email);
+			stmt.setString(5, address);
+			stmt.setString(6, phone);
+			stmt.setInt(7, 30);
+			stmt.setInt(8, 1);
+	
+			stmt.executeUpdate();
+				
 			return true;
 		}
 		
